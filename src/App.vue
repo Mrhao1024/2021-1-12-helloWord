ce36c674028f26593902ff438145638ccd63ed93
Mrhao1024 <mrhao1024@163.com>
2021-01-12 20:29:15 +0800

    第二次脚本提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
-   <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1821062512,1102029716&fm=26&gp=0.jpg"/>
+   <img src="https://gitee.com/Mrhao1024/blogimage/raw/master/img/20210112202729.jpg"/>
   </div>
 </template>
 
