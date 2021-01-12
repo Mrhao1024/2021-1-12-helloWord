4b3b2edf80472b94333329c94d299dcd25685874
Mrhao1024 <mrhao1024@163.com>
2021-01-12 20:09:56 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1821062512,1102029716&fm=26&gp=0.jpg"/>
   </div>
 </template>
 
